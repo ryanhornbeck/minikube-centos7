f817d5464339b1f476c4ab8c4cfb970df4ec29e6
Ryan Hornbeck <ryanhornbeck@gmail.com>
2021-06-16 15:58:49 -0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -28,7 +28,7 @@ yum install -y kubectl
 
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.27.0/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
 
-/usr/local/bin/minikube start
+minikube start
 
 kubectl run hello-minikube --image=k8s.gcr.io/echoserver:1.10 --port=8080
 kubectl expose deployment hello-minikube --type=NodePort
